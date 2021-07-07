f05e02c0d9399cf27204fecf02face7248059fc5
Adebayo Osinulu <bilardosinulu@gmail.com>
2021-07-07 21:20:49 +0100

    Nodejs-SQL Limit updated

--- demo_db_limit.js
+++ demo_db_limit.js
@@ -12,7 +12,9 @@ con.connect(function(err) {
   var sql = "SELECT * FROM customers LIMIT 5";
   //to select 5 but start from 3
   let query = "SELECT * FROM customers LIMIT 5 OFFSET 2"
-  con.query(query, function (err, result) {
+  //to select 5 but start from 3 with short hand, use limit <offsetfig>, <limitfig>
+  let query1 = "SELECT * FROM customers LIMIT 2,5"
+  con.query(query1, function (err, result) {
     if (err) throw err;
     console.log(result);
   });
